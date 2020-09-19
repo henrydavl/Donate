a21cdae1a1d039caf4017a86d5ec92180dc992c9
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2020-09-19 22:59:47 +0700

    update table seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,7 +15,7 @@ class UserTableSeeder extends Seeder
         $user = new User();
         $user->name = 'Henry';
         $user->email = 'henry@gmail.com';
-        $user->password = '$2y$10$E3OjImMSjPTG6J4SLgFWte1wyH7lZwEtfPiahDdT2LyZG/RjqTWuq'; #ekonom4r
+        $user->password = '$2y$10$E3OjImMSjPTG6J4SLgFWte1wyH7lZwEtfPiahDdT2LyZG/RjqTWuq';
         $user->role_id = 1; #root
         $user->isvalid = '1';
         $user->save();
@@ -23,7 +23,23 @@ class UserTableSeeder extends Seeder
         $user = new User();
         $user->name = 'Henry David';
         $user->email = 'henrydavid@gmail.com';
-        $user->password = '$2y$10$E3OjImMSjPTG6J4SLgFWte1wyH7lZwEtfPiahDdT2LyZG/RjqTWuq'; #ekonom4r
+        $user->password = '$2y$10$bZ0RLs.eo063CVtLrgcU.epgUXoIgF7Rh4Lrzzu/ElGC/OeQbQkKi'; #12345678
+        $user->role_id = 2; #admin
+        $user->isvalid = '1';
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Mychael';
+        $user->email = 'mychael@gmail.com';
+        $user->password = '$2y$10$bZ0RLs.eo063CVtLrgcU.epgUXoIgF7Rh4Lrzzu/ElGC/OeQbQkKi'; #12345678
+        $user->role_id = 1; #root
+        $user->isvalid = '1';
+        $user->save();
+
+        $user = new User();
+        $user->name = 'Mychael E';
+        $user->email = 'mychaele@gmail.com';
+        $user->password = '$2y$10$bZ0RLs.eo063CVtLrgcU.epgUXoIgF7Rh4Lrzzu/ElGC/OeQbQkKi'; #12345678
         $user->role_id = 2; #admin
         $user->isvalid = '1';
         $user->save();
